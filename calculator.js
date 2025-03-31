85e5247ea37d41dd061b2f386a8df1583aa1941a
Akin-Genc0 <akingenc212@gmail.com>
2025-03-31 12:54:35 +0100

    feat: refactor calculator to support dynamic expression evaluation and operator chaining
    Reworked calculator logic to evaluate full expressions using \eval()\, enabling support for chaining multiple operations. Added validation to prevent consecutive operators, improved backspace functionality, and included result rounding for long decimals. This approach replaces the original num1/op/num2 structure with a more flexible input string method.

--- calculator.js
+++ calculator.js
@@ -1,120 +1,133 @@
-const output = document.getElementById('text')
+const output = document.getElementById('text');
+const soutput = document.getElementById('stext');
 const btn = document.getElementById('num');
-const btnc = document.getElementById('c')
+const btnc = document.getElementById('c');
+const btnb = document.getElementById('b');
 
-const add = (x,y) => x + y;
- 
-const div = (x,y) => x / y;
+let currentNumber = ""; 
+let optr = ["+","-","/","*","%"];  
 
-const sub = (x,y) => x - y;
-
-const mult = (x,y) =>  x * y
-
-const mod = (x,y) => x % y;
-
-function operate(n1, op, n2){
-
-    if(op === "+"){
-        return add(n1,n2);
-    } else if(op === "-"){
-        return sub(n1,n2);
-    }else if(op === "*"){
-       return mult(n1,n2);
-    } else if(op === "/") {
-        return div(n1,n2);
-    } else if(op === "%") {
-        return mod(n1,n2);
-    }
-return 0;
-
-}
-
-
-let co = "";
-let currentNumber = "";
-let optr = ["+","-","/","*","%"]
 btn.addEventListener('click', function(e) {
+    let res = 0;
+    let err = "Error";
 
-    let num1 = 0;
-    let num2 = 0;
-    let ind = 0;
-    let opp = "";
-   let res = 0
-  let ans = 0;
-   
     currentNumber += e.target.textContent;
+
+    // Clicking white space does not trigger an error
+    if (!e.target.matches("button")) return;
+
     output.textContent = currentNumber;
-    
+
+    // Nested loop that goes through currentNumber and optr array
+    for (let i of currentNumber) {
+        for (let j of optr) {
+            
+            // Does a character from optr appear in currentNumber?
+            if (i === j) {
+                currentNumber.split(j); // Splits currentNumber on the position of j
+                res = eval(currentNumber); 
+                soutput.textContent = currentNumber;  
+                output.textContent = res;  
+
+            } 
+            // If result length is greater than 8 characters, round it
+            else if (res.toString().length > 8) {
+                output.textContent = res.toFixed(3); 
+
+            } 
+            // If the last and second-to-last values are operators, remove the last one
+            else if (optr.includes(currentNumber[currentNumber.length - 1]) &&
+                     optr.includes(currentNumber[currentNumber.length - 2])) {  
+                currentNumber = currentNumber.slice(0, -1);
+                output.textContent = currentNumber;
+            }  
+        }  
+    }
+
+    // Set error message if value is invalid
+    if (!isFinite(res)) {
+        output.textContent = err;  
+    }
+});
+
+// AC button resets values
+btnc.addEventListener('click', function() {  
+    soutput.textContent = "";   
+    res = 0;
+    currentNumber = "";
+    output.textContent = 0;
+});
+
+//Backspace button
+btnb.addEventListener('click', function() {  
+    currentNumber = currentNumber.slice(0, -1);
+    res = eval(currentNumber); 
+    soutput.textContent = currentNumber;  
+    output.textContent = res;  
+});
+
+
+
+
+
+//old way i was doin it
+
+/*
+
+
+
 
    for(let i = 0; i < currentNumber.length; i++){
         for(let j = 0; j < optr.length; j++){
-            if(currentNumber[i] === optr[j] ){ // if the value enterd is equal to a oprator then do somthing 
+            if(currentNumber[i] === optr[j] ){ 
              
-                opp = optr[j]; //gets oprator
-                ind = currentNumber.indexOf(optr[j]) +1; // gets the indexx of the oprator so wecan use it to find the seccond number
-                num1 =  currentNumber.slice(0, ind -1) ;  //anythimg from 0 index till that oprator is a num1 maybe stor that vlaue in a new varble
-                num1 = Number(num1); // convert to int
-             
-                
+                opp = optr[j];  
+                ind = currentNumber.indexOf(optr[j]) +1;  
+                num1 =  currentNumber.slice(0, ind -1) ; 
+                num2 = currentNumber.slice(ind, currentNumber.length -1) 
+                num1 = Number(num1);
+                num2 = Number(num2);
                } 
                
                else if(currentNumber[i] === "="){
                    
-                num2 = currentNumber.slice(ind, currentNumber.length -1) //gstarting index is the currectnumber up t the oprator and we add +1 so it skips it and we get het nmber nad not *88 instead we get 88
-                    num2 = Number(num2);
                     res  = operate(num1, opp, num2); 
-                    output.textContent = res;
-                    currentNumber = res;
+                    output.textContent =   res;
+                    currentNumber = res ;
                     num1 = res;
-                    opp = ""
+                    opp = "";
                     num2 = 0;  
-                   
-            } 
-       }
+                                  
+            }  else if(res.toString().length > 4){
+                output.textContent =   res.toFixed(3);
+            }   
+           
+    }
     
     }
-});
-
-
-btnc.addEventListener('click', function() {   
-   num1 = 0;
-   num2 = 0;
-   opp = "";
-   res = 0;
-   currentNumber = "";
-    output.textContent = 0;
-});
- 
-//give the = a id and if its pressed run the fucntion
-//if equal to '=' then run the oprator fucntion
 
 
 
-/*
- let n1 =  console.log(currentNumber[0]);
-   let o =   console.log(currentNumber[1]);
-   let n2 =   console.log(currentNumber[2]);
-
-    
 
 
 
-   if(currentNumber[currentNumber.length -1] === "="){
-   let res = operate(n1, o, n2)
-   output.textContent = res;
-   
-   }
-  
-  
-   if(currentNumber === "CA"){
-    console.log("test")
-   }
-*/
 
 
 
-/*
- console.log(i.pop())
-                num1 = currentNumber;//anythimg from 0 index till that oprator is a num1 maybe stor that vlaue in a new varble
-                console.log(num1)
-                */
+
+ else if(currentNumber[i] === "="){
+                   
+                num2 = currentNumber.slice(ind, currentNumber.length -1) //gstarting index is the currectnumber up t the oprator and we add +1 so it skips it and we get het nmber nad not *88 instead we get 88
+                    num2 = Number(num2); // convert to int
+                    res  = operate(num1, opp, num2); 
+                    output.textContent =   res;
+                    currentNumber = res ;
+                    num1 = res;
+                    opp = "";
+                    num2 = 0;  
+                                  
+            }  else if(res.toString().length > 4){
+                output.textContent =   res.toFixed(3);
+            } 
+
+            */
